51edef2955cc744fc2b7a2e12254f04e636f90a3
Vick Scarlet <scarlet_vick@outlook.com>
2022-08-12 21:06:35 +0800

    build: copy CNAME

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     vue(),
     legacy({
       targets: ['defaults', 'not IE 11']
+    }),
+    viteStaticCopy({
+      targets: [
+        { src: 'CNAME', dest: '.' },
+      ]
     })
   ],
   build: {
