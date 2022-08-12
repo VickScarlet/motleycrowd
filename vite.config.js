07c9d74f96575d5aff0bcfd977def10769340ccd
Vick Scarlet <scarlet_vick@outlook.com>
2022-08-12 20:52:21 +0800

    build: add polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    sourcemap: true,
   },
 })
