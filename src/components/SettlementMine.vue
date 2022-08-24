0de826eee2083f67b6a751716c28a35747d877e4
Vick Scarlet <scarlet_vick@outlook.com>
2022-08-25 00:10:21 +0800

    feature: settlement mine info and ranking, score

--- src/components/SettlementMine.vue
+++ src/components/SettlementMine.vue
@@ -1,12 +1,17 @@
 <script setup>
 import AnswerLineBarChart from './AnswerLineBarChart.vue'
+import UserCard from './UserCard.vue'
 </script>
 <template>
     <div class="container">
         <div class="chart">
             <AnswerLineBarChart :answers="answers" :width="330" :height="330"/>
         </div>
-        <div class="info"> </div>
+        <ul class="info">
+            <li><UserCard :user="info" /></li>
+            <li>排名: {{ranking}}</li>
+            <li>得分: {{score}}</li>
+        </ul>
     </div>
 </template>
 <script>
@@ -16,6 +21,9 @@ export default defineComponent({
     data() {
         return {
             answers: [],
+            ranking: -1,
+            score: 0,
+            info: {},
         }
     },
     mounted() {
@@ -26,6 +34,9 @@ export default defineComponent({
         update() {
             const mine = this.getData();
             if(!mine) return;
+            this.info = mine.info;
+            this.ranking = mine.ranking;
+            this.score = mine.score;
             this.answers = mine.map(({value, answer}, index)=>({
                 name: `Q${index+1}`,
                 value, answer
@@ -51,4 +62,30 @@ export default defineComponent({
     left: 0;
     top: 0;
 }
+
+.info {
+    height: 100%;
+    width: calc(100% - 300px);
+    > li {
+        position: relative;
+        &:first-child {
+            height: 100px;
+            width: 300px;
+            margin: auto;
+        }
+    }
+}
+@media screen and (max-width: 680px){
+    .container {
+        flex-direction: column;
+    }
+    .chart {
+        margin: auto;
+    }
+    .info {
+        height: auto;
+        width: 100%;
+    }
+}
+
 </style>
